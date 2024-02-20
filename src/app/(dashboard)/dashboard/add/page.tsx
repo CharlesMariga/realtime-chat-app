9efe750fc74c75559655b0249757adb778e5b31f
CharlesMariga <charlesmariga37@gmail.com>
2024-02-20 13:03:35 +0300

    feat: finishing the add friend feature

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,13 @@
+import AddFriendButton from "@/components/AddFriendButton";
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="pt-8">
+      <h1 className="mb-8 text-5xl font-bold">Add a friend</h1>
+      <AddFriendButton />
+    </main>
+  );
+};
+
+export default page;
